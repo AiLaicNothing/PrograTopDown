6e2fd09cd85ce0950c9354a4e42acec39cc03e37
Drocaud <megalapiz111@gmail.com>
2026-05-19 23:51:32 -0500

    Enemy Dasher terminado
    Se crearon 2 scripts Detect y DashEnemy
    
    Detect: Sirve para detectar al player y habital el dash que esta en el DashEnemy.
    DashEnemy: Es donde esta el movimiento, el dash que necesita un bool para funcione, etc.

---
+++ My project/Assets/Scripts/Piero/DashEnemy.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class DashEnemy : EnemyBase
+{
+    
+    [SerializeField] private Detect detect;
+    [SerializeField] private int speed;
+    [SerializeField] private float speedDash;
+
+    private int recoil;
+    private GameObject player;
+    private Rigidbody rbEnemy;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rbEnemy = GetComponent<Rigidbody>();
+        detect = GetComponentInChildren<Detect>();
+        recoil = 16;
+    }
+
+    void Update()
+    {
+        EnemyMovement();
+        ActivateDash();    
+    }
+
+    private void Dash()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        rbEnemy.AddForce(direction * speedDash, ForceMode.Impulse);
+        Debug.Log("Dash finished!");
+        detect.controllerDash = false;
+    }
+
+    private void EnemyMovement()
+    {
+        if (player != null && detect.controllerDash == false)
+        {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rbPlayer = player.GetComponent<Rigidbody>();
+            if (rbPlayer != null)
+            {
+                Vector3 direction = (collision.transform.position - transform.position).normalized;
+                rbPlayer.AddForce(direction * recoil, ForceMode.Impulse);
+                Debug.Log("Player hit by dash enemy!"); 
+            }
+                     
+        }
+    }
+
+    private void ActivateDash()
+    {
+       if (detect.controllerDash == true)
+        {
+            Dash();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            TakeDamage(10f);
+            Debug.Log("Enemy took damage from bullet!");
+        }
+    }
+}
